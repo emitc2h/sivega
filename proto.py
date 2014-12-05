ed9296436da3a1198f052459653a741e661ea388
Michel Trottier-McDonald <mta58@sfu.ca>
2014-12-05 12:15:43 +0100

    Adding logo for repository front page

--- proto.py
+++ proto.py
@@ -1,7 +1,7 @@
 import cairosvg, lxml
 
-f_in = open('test.svg')
+f_in = open('logo.svg')
 svg_code = f_in.read()
-f_out = open('test.png', 'w')
+f_out = open('logo.png', 'w')
 cairosvg.svg2png(bytestring=svg_code, write_to=f_out)
 f_out.close()
