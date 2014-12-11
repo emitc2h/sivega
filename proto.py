8f9b64064a473ce8815d19e6a364bb6c9aa2911b
Michel Trottier-McDonald <mta58@sfu.ca>
2014-12-11 18:04:07 +0100

    structure

--- proto.py
+++ proto.py
@@ -1,7 +1,10 @@
-import cairosvg, lxml
+import cairosvg
 
 f_in = open('logo.svg')
 svg_code = f_in.read()
-f_out = open('logo.png', 'w')
-cairosvg.svg2png(bytestring=svg_code, write_to=f_out)
-f_out.close()
+f_png_out = open('test.png', 'w')
+f_pdf_out = open('test.pdf', 'w')
+cairosvg.svg2png(bytestring=svg_code, write_to=f_png_out)
+cairosvg.svg2pdf(bytestring=svg_code, write_to=f_pdf_out)
+f_png_out.close()
+f_pdf_out.close()
